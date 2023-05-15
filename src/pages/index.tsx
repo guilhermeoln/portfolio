408bb1687d43124d51da985ad6982886367b8409
jose guilherme <guilhermedev2002@gmail.com>
2023-05-15 14:52:56 -0300

    feat(home): Criação da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <Flex width="100vw" minH="100vh">
+      <Flex width="20vw"></Flex>
+      <Flex width="80vw"></Flex>
+    </Flex>
+  );
 }
