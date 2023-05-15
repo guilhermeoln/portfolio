0a1edafa675d5a566e1f6f4fcdcb814458fd2316
jose guilherme <guilhermedev2002@gmail.com>
2023-05-15 15:37:58 -0300

    fix(Project): Ajustando projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import SideBar from "@/components/SideBar";
+import { iceColor } from "@/styles/variaveis";
 import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Flex width="100vw" minH="100vh">
-      <Flex width="20vw"></Flex>
-      <Flex width="80vw"></Flex>
+      <Flex width="20vw">
+        <SideBar />
+      </Flex>
+      <Flex width="80vw" bg={iceColor}></Flex>
     </Flex>
   );
 }
